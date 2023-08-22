6b2b8826f2c09f5b6fe92796c011a5607bd1e989
VEEKSHITHSUMANTH G <gurramveekshithsumanth@gmail.com>
2023-08-22 18:52:14 +0530

    remove unnecessary console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,9 @@
-console.log('client side js file is loaded')
-
-// selecting our form element and adding eventlistener submit
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
-//always sleects the first matched type of element
-// const messageOne=document.querySelector('p')
-// '.className' in case of selecting class
-// '#id' in case of particular id
 const messageOne=document.querySelector('#message-1')
 const messageTwo=document.querySelector('#message-2')
 
-// to change the text,use textContent property
-
-
 weatherForm.addEventListener('submit',(e)=>{
-    // prevents form from reloading(default behavior)
     e.preventDefault()
 
     const location=search.value
